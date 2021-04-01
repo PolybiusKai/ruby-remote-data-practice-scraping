91e3ddf926c670103889a074a0df60c20b80854d
rsmith <legendary.belief@gmail.com>
2021-03-31 19:45:50 -0700

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,7 +7,7 @@ html2 = open("https://flatironschool.com/our-courses/")
 doc = Nokogiri::HTML(html)
 doc2 = Nokogiri::HTML(html2)
 
-#puts doc.css(".headline-26OIBN").text.strip  
+puts doc.css(".headline-26OIBN").text.strip  
 puts doc2.css(".text-4GLMvr")[5].children.first.text
 courses = doc2.css(".text-4GLMvr")
 
